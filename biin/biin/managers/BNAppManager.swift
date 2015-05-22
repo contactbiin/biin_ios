30056881df1df815b3f101c83e053532b05b4b18
Esteban Padilla <ep@estebanpadilla.com>
2015-05-22 15:49:25 -0600

    Save chances.
    Save chances.

--- biin/biin/managers/BNAppManager.swift
+++ biin/biin/managers/BNAppManager.swift
@@ -10,7 +10,7 @@ struct BNAppSharedManager { static let instance = BNAppManager() }
 
 class BNAppManager {
     
-    var IS_PRODUCTION_RELEASE = false
+    var IS_PRODUCTION_RELEASE = true
     
     var counter = 0
     var version = "0.1.8"
