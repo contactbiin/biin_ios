0a6a3070e72b956657b1ecc2d092ef8d90b7e398
Esteban Padilla <ep@estebanpadilla.com>
2015-06-26 09:44:29 -0600

    Change iPhone 6 plus view.
    Change iPhone 6 plus view.

--- biin/biin/views/categories/BiinieCategoriesView_SitesContainer.swift
+++ biin/biin/views/categories/BiinieCategoriesView_SitesContainer.swift
@@ -206,8 +206,8 @@ class BiinieCategoriesView_SitesContainer: BNView, UIScrollViewDelegate {
             columns = 2
             break
         case .iphone6Plus:
-            siteViewWidth = (SharedUIManager.instance.screenWidth - 40) / 3
-            siteViewHeight = SharedUIManager.instance.screenHeight / 4
+            siteViewWidth = (SharedUIManager.instance.screenWidth - 30) / 2
+            siteViewHeight = SharedUIManager.instance.screenHeight / 3
             columns = 3
             break
         case .ipad:
