3001dc08042db60ceffc918d9ad822b42d291d71
Esteban Padilla <ep@estebanpadilla.com>
2016-04-07 15:26:01 -0600

    Saving.
    Saving.

--- biin/biin/viewControllers/SingupViewController.swift
+++ biin/biin/viewControllers/SingupViewController.swift
@@ -111,7 +111,10 @@ class SingupViewController:UIViewController, UIPopoverPresentationControllerDele
         loginBtn!.setTitleColor(UIColor.darkGrayColor(), forState: UIControlState.Normal)
         self.view.addSubview(loginBtn!)
 
-
+        fade = UIView(frame: CGRectMake(0, 0, screenWidth, screenHeight))
+        fade!.backgroundColor = UIColor.blackColor()
+        fade!.alpha = 0
+        self.view.addSubview(fade!)
 
         loginView = LoginView(frame:CGRectMake(screenWidth, 0, screenWidth, screenHeight))
         loginView!.delegate = self
@@ -137,11 +140,6 @@ class SingupViewController:UIViewController, UIPopoverPresentationControllerDele
         }
         
         singupBtn!.frame.origin.y = ypos
-        
-        fade = UIView(frame: CGRectMake(0, 0, screenWidth, screenHeight))
-        fade!.backgroundColor = UIColor.blackColor()
-        fade!.alpha = 0
-        self.view.addSubview(fade!)
         
         //self.view.layerGradient()
     }
